ad0fb674e6dfaef25e845aedd76f8e9aa0d988c2
sergiofdezperea <sergiofdezperea@gmail.com>
2023-11-12 22:17:50 +0100

    Add experiences JSON data and update Experiences component and CSS

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,43 +1,14 @@
 import { useState, useEffect } from "react"
 import formatToList from "./formatToList"
 // This component will return a block of html with the sctructure of a json
-
-const exampleJson = {
-  experiences: [
-    {
-      company: "Metrohm DropSens",
-      jobTitle: "Desarrollador Java",
-      companyDescription:
-        "Fabricación de electrodomésticos, equipamiento eléctrico y electrónica",
-      startDate: "jul. 2023",
-      endDate: "Actualidad",
-      accomplishmentList: ["accomplishment1", "accomplishment2"],
-    },
-    {
-      company: "Company Name",
-      jobTitle: "Job Title",
-      companyDescription: "Company Description",
-      startDate: "Start Date",
-      endDate: "End Date",
-      accomplishmentList: ["accomplishment1", "accomplishment2"],
-    },
-    {
-      company: "Company Name2",
-      jobTitle: "Job Title2",
-      companyDescription: "Company Description2",
-      startDate: "Start Date2",
-      endDate: "End Dat2e",
-      accomplishmentList: ["accomplishment1", "accomplishment2"],
-    },
-  ],
-}
-
+const experiencesJson = await fetch("/assets/json/experiences.json")
+const data = await experiencesJson.json()
 function Experiences() {
   const [experiences, setExperiences] = useState([])
   const experienceTitle = "Responsabilidades"
 
   useEffect(() => {
-    setExperiences(exampleJson.experiences)
+    setExperiences(data.experiences)
   }, [])
 
   console.log("Experiences.jsx: experiences: ", experiences)
@@ -51,7 +22,7 @@ function Experiences() {
             ? formatToList(experienceTitle, experience.accomplishmentList)
             : null
           return (
-            <div key={index}>
+            <div key={index} className="experience">
               <h3>
                 {experience.jobTitle} ({experience.company})
               </h3>
