047a6b7c1748300b5acabd2eb6afc5703f469af5
sergiofdezperea <sergiofdezperea@gmail.com>
2023-11-14 00:27:03 +0100

    Add social media icons and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Main from "./components/Main"
+
 import { Fragment } from "react"
 
+const siteProps = {
+  email: "sergiofdezperea@gmail.com",
+  gitHub: "sergiofdezperea",
+  linkedIn: "sergio-fernandez-perea",
+}
 function App() {
   return (
     <Fragment>
       <Header />
       <Main />
+      <Footer {...siteProps} />
     </Fragment>
   )
 }
